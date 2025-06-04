b74da0b71dab1d509b026958f1c06fa8816cdd79
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-04 17:57:12 +0300

    final

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,10 +2,4 @@ require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.28",
-  networks: {
-    hell: {
-      url: "http://127.0.0.1:8545",
-      accounts: [process.env.PRIVATE_KEY] // Set in GitHub Secrets
-    }
-  }
 };
