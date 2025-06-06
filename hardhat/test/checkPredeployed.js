6fd3acd65a31d4ef64ee2ea135f56b89d88f5166
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-06 14:50:15 +0300

    dadasd

--- hardhat/test/checkPredeployed.js
+++ hardhat/test/checkPredeployed.js
@@ -3,16 +3,15 @@ const { ethers } = require("hardhat");
 
 describe("Lock (Predeployed)", function () {
   it("Should read state from predeployed contract", async function () {
-    const lockAddress = process.env.LOCK_ADDRESS;
-    expect(lockAddress).to.match(/^0x[a-fA-F0-9]{40}$/); // basic sanity check
+    const address = process.env.LOCK_ADDRESS;
+
+    console.log("Loaded LOCK_ADDRESS:", address);
+    expect(address).to.match(/^0x[a-fA-F0-9]{40}$/);
 
     const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.attach(lockAddress);
+    const lock = await Lock.attach(address);
 
     const unlockTime = await lock.unlockTime();
-    const owner = await lock.owner();
-
     expect(unlockTime).to.be.a("bigint");
-    expect(owner).to.properAddress;
   });
 });
