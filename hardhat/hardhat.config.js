9da31bdbd51c451aa7e2b59137bb533c6303e9f4
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-06 16:58:39 +0300

    dasdad

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     localhost: {
       url: "http://localhost:8545",
       accounts: [
-        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+        "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113b37a2a85c6e4b8fdc5b8a7e6"
       ],
     },
   },
