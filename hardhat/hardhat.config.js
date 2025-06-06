3901bb1a3c7cccc96605af2d1e58ad5920900566
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-06 17:13:24 +0300

    dadsadasdadas

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,13 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const DEPLOYER_KEY = process.env.HARDHAT_DEPLOYER_KEY;
+
 module.exports = {
   solidity: "0.8.28",
   networks: {
     localhost: {
       url: "http://localhost:8545",
-      accounts: [
-        "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113b37a2a85c6e4b8fdc5b8a7e6"
-      ],
+      accounts: DEPLOYER_KEY ? [DEPLOYER_KEY] : [],
     },
   },
 };
