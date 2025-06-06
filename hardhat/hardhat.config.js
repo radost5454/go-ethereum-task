3e3e370e3441599a09d3952071a7de97ec5d8c14
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-06 22:30:31 +0300

    adsaasdas

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,7 +5,7 @@ const DEPLOYER_KEY = "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113b37a2a85c6e4b8fd
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    localhost: {
+    geth: {
       url: "http://127.0.0.1:8545",
       accounts: [DEPLOYER_KEY],
     },
