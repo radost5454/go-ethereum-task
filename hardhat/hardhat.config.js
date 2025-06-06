f84dbf2af22e5a5a8ebd6ff900207ac9096d7ee5
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-06 16:42:04 +0300

    sasaassa

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,10 +5,9 @@ module.exports = {
   networks: {
     localhost: {
       url: "http://localhost:8545",
-    },
-    devnet: {
-      url: "http://localhost:8545",
-      chainId: 1337,
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+      ],
     },
   },
 };
