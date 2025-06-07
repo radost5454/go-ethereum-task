6ad952c57a9a8f572ef150a4c33bfff3503f9db7
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-07 13:02:34 +0300

    rrr

--- hardhat/test/test.js
+++ hardhat/test/test.js
@@ -1,18 +1,24 @@
-const { ethers } = require("hardhat");
+const { JsonRpcProvider, Wallet, Contract } = require("ethers");
 const { expect } = require("chai");
+const LockArtifact = require("../artifacts/contracts/Lock.sol/Lock.json"); // adjust if needed
 
 describe("Lock Contract (Live Devnet)", function () {
   let lock;
 
   before(async function () {
-    const contractAddress = "0xYourDeployedLockAddress"; // Use actual deployed address
-    const Lock = await ethers.getContractFactory("Lock");
-    lock = Lock.attach(contractAddress);
+    const provider = new JsonRpcProvider("http://localhost:8545");
+    const signer = new Wallet(
+      "0x59c6995e998f97a5a004497e5da0da3eb20c29e0cde8c3c2d20c3eab366d8e84",
+      provider
+    );
+
+    const deployedAddress = "0xYourDeployedLockAddress"; // update this!
+    lock = new Contract(deployedAddress, LockArtifact.abi, signer);
   });
 
   it("Should return unlockTime", async function () {
     const unlockTime = await lock.unlockTime();
-    console.log("Unlock time:", unlockTime.toString());
+    console.log("🔓 Unlock time:", unlockTime.toString());
     expect(unlockTime).to.be.a("bigint");
   });
 });
