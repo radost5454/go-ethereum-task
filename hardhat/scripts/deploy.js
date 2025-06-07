9b690770fa7b04e2bf1ee8e2b5f5476611c623a4
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-07 10:50:32 +0300

    testttt

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,13 +1,16 @@
 async function main() {
     const [deployer] = await ethers.getSigners();
+  
     console.log("Deploying with account:", deployer.address);
-    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    // Get balance using the provider
+    const balance = await ethers.provider.getBalance(deployer.address);
+    console.log("Account balance:", balance.toString());
   
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello from Geth!");
-    await greeter.deployed();
   
-    console.log("Greeter deployed to:", greeter.address);
+    console.log("Greeter deployed to:", greeter.target); // .target is the address in Ethers v6
   }
   
   main().catch((error) => {
