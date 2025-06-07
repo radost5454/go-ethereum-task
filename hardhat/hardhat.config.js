eceb89ac81de7abce89f33e9a5641099916fe4ec
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-07 12:10:22 +0300

    update

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,25 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("./tasks/accounts");
 
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.28",
-      },
-      {
-        version: "0.8.24",
-      },
-      {
-        version: "0.8.0",
-      }
-    ]
-  },
+  solidity: "0.8.24",
   networks: {
     geth: {
       url: "http://localhost:8545",
       accounts: [
-        // Replace with real key in secure settings for production
         "0x59c6995e998f97a5a004497e5da0da3eb20c29e0cde8c3c2d20c3eab366d8e84"
       ]
     }
