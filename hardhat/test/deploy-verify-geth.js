c8cf8904cc880e386578fa229a936687ea741365
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-08 13:16:00 +0300

    dadada

--- hardhat/test/deploy-verify-geth.js
+++ hardhat/test/deploy-verify-geth.js
@@ -4,13 +4,14 @@ const { ethers } = require("hardhat");
 describe("Lock - Geth Verification", function () {
   it("Should read unlockTime from already deployed Lock contract", async function () {
     const contractAddress = process.env.CONTRACT_ADDRESS;
+    console.log("🔍 CONTRACT_ADDRESS from env:", contractAddress);
 
     expect(contractAddress).to.match(/^0x[a-fA-F0-9]{40}$/);
 
     const lock = await ethers.getContractAt("Lock", contractAddress);
     const unlockTime = await lock.unlockTime();
+    console.log("🕒 Unlock time:", unlockTime.toString());
 
-    console.log("Unlock time:", unlockTime.toString());
     expect(unlockTime).to.be.a("BigNumber");
   });
 });
