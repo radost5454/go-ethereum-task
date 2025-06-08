e3c293dd4a01a64c9fd1eebc59cf1b375e68e115
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-08 15:28:33 +0300

    aaaa

--- hardhat/test/deploy-verify-geth.js
+++ hardhat/test/deploy-verify-geth.js
@@ -1,24 +1,41 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Lock - Geth Verification", function () {
-  it("Should read unlockTime from already deployed Lock contract", async function () {
+describe("Lock", function () {
+  let lock;
+  let unlockTime;
+  let lockedAmount = 1_000_000_000;
+
+  beforeEach(async function () {
     const contractAddress = process.env.CONTRACT_ADDRESS;
     console.log("🔍 CONTRACT_ADDRESS from env:", contractAddress);
     expect(contractAddress).to.match(/^0x[a-fA-F0-9]{40}$/);
 
-    // Get the contract factory to ensure we have the correct ABI
     const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.attach(contractAddress);
+    lock = await Lock.attach(contractAddress);
     
     // Verify the contract exists
     const code = await ethers.provider.getCode(contractAddress);
     expect(code).to.not.equal("0x", "Contract does not exist at the specified address");
     
-    // Try to read the unlockTime
-    const unlockTime = await lock.unlockTime();
+    unlockTime = await lock.unlockTime();
     console.log("📆 Unlock time:", unlockTime.toString());
+  });
 
-    expect(typeof unlockTime).to.equal("bigint");
+  it("Should set the right unlockTime", async function () {
+    expect(await lock.unlockTime()).to.equal(unlockTime);
+  });
+
+  it("Should not allow withdrawals before unlock time", async function () {
+    await expect(lock.withdraw()).to.be.revertedWith(
+      "You can't withdraw yet"
+    );
+  });
+
+  it("Should not allow withdrawals by non-owner", async function () {
+    const [_, otherAccount] = await ethers.getSigners();
+    await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+      "You aren't the owner"
+    );
   });
 });
