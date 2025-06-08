9d0fb6355884dd67497c2b95987cbcf9812e7dbd
Radostina Lyubomirova <radostina.lyubomirova@amusnet.com>
2025-06-08 15:38:58 +0300

    dasdada

--- hardhat/test/deploy-verify-geth.js
+++ hardhat/test/deploy-verify-geth.js
@@ -4,13 +4,18 @@ const { ethers } = require("hardhat");
 describe("Lock", function () {
   let lock;
   let unlockTime;
-  let lockedAmount = 1_000_000_000;
+  let owner;
+  let otherAccount;
 
   beforeEach(async function () {
     const contractAddress = process.env.CONTRACT_ADDRESS;
     console.log("🔍 CONTRACT_ADDRESS from env:", contractAddress);
     expect(contractAddress).to.match(/^0x[a-fA-F0-9]{40}$/);
 
+    // Get signers
+    [owner, otherAccount] = await ethers.getSigners();
+
+    // Get the contract factory to ensure we have the correct ABI
     const Lock = await ethers.getContractFactory("Lock");
     lock = await Lock.attach(contractAddress);
     
@@ -27,13 +32,12 @@ describe("Lock", function () {
   });
 
   it("Should not allow withdrawals before unlock time", async function () {
-    await expect(lock.withdraw()).to.be.revertedWith(
+    await expect(lock.connect(owner).withdraw()).to.be.revertedWith(
       "You can't withdraw yet"
     );
   });
 
   it("Should not allow withdrawals by non-owner", async function () {
-    const [_, otherAccount] = await ethers.getSigners();
     await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
       "You aren't the owner"
     );
